d1fadf8e5b8c92e762630f178ef4bd8606d64279
engineer-dilnawaz <155418947+engineer-dilnawaz@users.noreply.github.com>
2025-05-04 17:46:24 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { Link, NavLink } from "react-router-dom";
+import viteLogo from "/vite.svg";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between px-4 py-4 shadow-md md:px-8">
+      <Link to="/">
+        <img src={viteLogo} alt="app logo" />
+      </Link>
+      <ul className="flex gap-4">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-blue-400 underline" : ""
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "text-blue-400 underline" : ""
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "text-blue-400 underline" : ""
+            }
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
+import Error from "./components/Error.jsx";
+import Contact from "./components/Contact.jsx";
+import About from "./components/About.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
